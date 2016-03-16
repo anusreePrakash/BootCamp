8878d9ce1bfd395e532c513505b433f573740a76
Anusree <anusree@thoughtworks.com>
2016-03-16 17:41:37 +0530

    Deleted unwanted tests and added job of the class

--- src/march_16/Rectangle.java
+++ src/march_16/Rectangle.java
@@ -1,23 +1,33 @@
 package march_16;
 
-public class Rectangle {
-    private final int height;
-    private final int width;
+/*
+    Job of Rectangle class:
+        - Anything related to height and breadth
+            - Calculate the area
+            - Calculate the perimeters
+ */
 
-    public Rectangle(int width, int height) {
+public class Rectangle {
+    private final double height;
+    private final double width;
+
+    public Rectangle(double width, double height) {
         this.width = width;
         this.height = height;
     }
 
-    public int calcArea() {
-        return height*width;
+    public double calculateArea() {
+        return height * width;
     }
 
-    public static Rectangle create(int width, int height) {
-        if(width<=0)
-            throw new IllegalArgumentException("Width should be a positive Integer greater than zero");
-        if(height<=0)
-            throw new IllegalArgumentException("Height should be a positive Integer greater than zero");
+    public static Rectangle create(double width, double height) {
+        if(width<=0 || height <=0){
+            throw new IllegalArgumentException("illegal arguments");
+        }
         return new Rectangle(width,height);
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (width + height);
     }
 }
--- test/march_16/RectangleTest.java
+++ test/march_16/RectangleTest.java
@@ -7,26 +7,14 @@ import static junit.framework.TestCase.assertEquals;
 public class RectangleTest {
 
     @Test
-    public void testRectangleCanCalculateItsArea() throws Exception {
+    public void testRectangleCanCalculateItsArea() throws IllegalArgumentException {
         Rectangle rectangle = Rectangle.create(10,4);
-        assertEquals(40,rectangle.calcArea());
-    }
-    @Test
-    public void testRectangleShouldThrowExceptionForNegativeWidth(){
-        try{
-            Rectangle rectangle = Rectangle.create(-10,4);
-        }catch(IllegalArgumentException err) {
-            assertEquals(err.getMessage(), "Width should be a positive Integer greater than zero");
-        }
-    }
-    @Test
-    public void testRectangleShouldThrowExceptionForNegativeHeight(){
-        try{
-            Rectangle rectangle = Rectangle.create(10,-4);
-        }catch(IllegalArgumentException err) {
-            assertEquals(err.getMessage(), "Height should be a positive Integer greater than zero");
-        }
+        assertEquals(40d,rectangle.calculateArea());
     }
 
-
+    @Test
+    public void testRectangleCanCalculateItsPerimeter() throws IllegalArgumentException {
+        Rectangle rectangle = Rectangle.create(10,4);
+        assertEquals(28d,rectangle.calculatePerimeter());
+    }
 }
